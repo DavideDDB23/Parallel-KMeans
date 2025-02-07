f4f03f89b8d6951ba628057ed1dae8f000b34013
davide <davidedeblasio2003@gmail.com>
2025-02-07 12:44:24 +0100

    generate for cluster

--- generate_data.py
+++ generate_data.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-import numpy as np
-import sys
+import random
 
 def generate_new_file(output_file, num_points, num_dims):
-    new_data = np.random.randint(-100, 101, size=(num_points, num_dims))
+    with open(output_file, 'w') as file:
+        for _ in range(num_points):
+            # Generate a row of random integers between -100 and 100.
+            row = [str(random.randint(-100, 100)) for _ in range(num_dims)]
+            # Write the row to the file as tab-separated values.
+            file.write('\t'.join(row) + '\n')
     
-    # Save the generated data to the output file with tab-separated values.
-    np.savetxt(output_file, new_data, fmt='%d', delimiter='\t')
     print(f"Generated file '{output_file}' with {num_points} points and {num_dims} dimensions.")
 
 def main():
-    output_file = '3200k_100.inp'
-    num_points = 3200000
+    output_file = '1600k_100.inp'
+    num_points = 1600000
     num_dims = 100
 
     generate_new_file(output_file, num_points, num_dims)
